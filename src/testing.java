9b7c33b5e33776d3010daf7472a6958531ae4321
Erkan <erkan.ec.cicek@gmail.com>
2022-06-23 21:38:53 +0200

    finished the color table!

--- src/testing.java
+++ src/testing.java
@@ -2,68 +2,53 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class testing {
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.setSize(500,700);
+		frame.setSize(500,500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-		DefaultTableModel model = new DefaultTableModel(){
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return false;
-			}
-		};
-
+		DefaultTableModel model = new DefaultTableModel();
 		JTable table = new JTable(model);
+
 		model.addColumn("COLORS");
-		model.addRow(new Object[]{"hi"});
-		model.addRow(new Object[]{"ok"});
-
-
-		TableColumn column;
-		myRenderer renderer = new myRenderer();
-		column = table.getColumnModel().getColumn(0);
-		column.setCellRenderer(renderer);
-
-
+		model.addRow(new Object[]{"255,0,0"});
+		model.addRow(new Object[]{"0,255,0"});
+		model.addRow(new Object[]{"0,0,255"});
 		JScrollPane scrollPane = new JScrollPane(table);
+
+		TableColumnModel tcm = table.getColumnModel();
+		TableColumn tm = tcm.getColumn(0);
+		tm.setCellRenderer(new Renderer());
+
 		frame.add(scrollPane);
-
 		frame.setVisible(true);
-
-		Scanner scanner = new Scanner(System.in);
-		int x = scanner.nextInt();
-		if (x == 1){
-			renderer.setColor(Color.blue);
-			model.addRow(new Object[]{"NICE"});
-		}
 	}
 }
 
-class myRenderer extends DefaultTableCellRenderer{
-	Color bg, fg;
-	public myRenderer() {
-		super();
-		this.bg = Color.green;
-		this.fg = Color.black;
-	}
-	public Component getTableCellRendererComponent(JTable table, Object
-			value, boolean isSelected, boolean hasFocus, int row, int column)
-	{
-		Component cell = super.getTableCellRendererComponent(table, value,
-				isSelected, hasFocus, row, column);
-		cell.setBackground(bg);
-		cell.setForeground(fg);
-		return cell;
-	}
+class Renderer extends DefaultTableCellRenderer{
+	@Override
+	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		setHorizontalAlignment(CENTER);
+		String tempString = (String) table.getValueAt(row, column);
+		String[]test = tempString.split(",");
 
-	public void setColor(Color color){
-		this.bg = color;
+		Color color = new Color(
+				Integer.parseInt(test[0]),
+				Integer.parseInt(test[1]),
+				Integer.parseInt(test[2])
+		);
+
+		setBackground(color);
+
+		super.getTableCellRendererComponent(table, value,isSelected, hasFocus, row, column);
+
+		return this;
 	}
 }
+
+
 
